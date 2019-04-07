80912236078fbe0a772ac1ce942b0ec14de194fb
Tricia Medina <tricia.medina@gmail.com>
2019-04-07 13:28:34 -0700

    enter item functionality added and event delegation adjusted

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 $(function() {
-   
-    $("#js-shopping-list-form").submit( function(event){
-        alert("Files are linked!");
-        event.preventDefault();
-    });
-
-    $(".shopping-item-toggle").click( function(event){
+    
+    $("ul").on("click", ".shopping-item-toggle", function(event){
         $(this)
         .closest("li").children(".shopping-item")
         .toggleClass("shopping-item__checked");
     });
-
-    $(".shopping-item-delete").click( function(event){
+   
+    $("ul").on("click", ".shopping-item-delete", function(event){
         $(this)
         .closest("li")
         .remove();
+    });
+
+    $("#js-shopping-list-form").submit( function(event){
+        
+        let entry = $("#shopping-list-entry").val();
+       
+        let newItem = 
+        `<span class="shopping-item">${entry}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+        </div>`;
+        
+        $(".shopping-list").prepend(`<li>${newItem}</li>`);
+
+        $(this).find("input[type=text], textarea").val("");
+
+        event.preventDefault();
     });
 
 });
