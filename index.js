f1a15e14798a85a7dfdebb7a9653882ad5024057
Tricia Medina <tricia.medina@gmail.com>
2019-04-07 12:49:19 -0700

    check button functionality added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function testScript() {
+$(function() {
+   
     $("#js-shopping-list-form").submit( function(event){
         alert("Files are linked!");
         event.preventDefault();
-    })
-}
+    });
 
-$(testScript)
+    $(".shopping-item-toggle").click( function(event){
+        $(this)
+        .closest("li").children(".shopping-item")
+        .toggleClass("shopping-item__checked");
+    });
+
+    $(".shopping-item-delete").click( function(event){
+        alert("Delete button working");
+    });
+
+});
